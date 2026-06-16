ff1129bbba61d09e4ae28d35729e5b33f0f26c33
Official <neethukrishnan020@gmail.com>
2026-06-16 16:05:05 +0530

    Add trained model for deployment

--- src/train_model.py
+++ src/train_model.py
@@ -57,46 +57,45 @@ def train_model(df):
         print("\nAverage ROC-AUC:")
         print(scores.mean())
 
-    model.fit(X_train,y_train,eval_set=[(X_test, y_test)],eval_metric='auc')
-    y_prob = model.predict_proba(X_test)[:, 1]
+        model.fit(X_train,y_train,eval_set=[(X_test, y_test)],eval_metric='auc')
+        y_prob = model.predict_proba(X_test)[:, 1]
 
-    ## Threshold tuning
-    precisions, recalls, thresholds = precision_recall_curve(y_test,y_prob)
-    f1_scores = (2 * precisions * recalls /(precisions + recalls + 1e-8))
-    best_idx = np.argmax(f1_scores)
-    # best_threshold = thresholds[best_idx]
-    best_threshold = 0.45
+        ## Threshold tuning
+        precisions, recalls, thresholds = precision_recall_curve(y_test,y_prob)
+        f1_scores = (2 * precisions * recalls /(precisions + recalls + 1e-8))
+        best_idx = np.argmax(f1_scores)
+        # best_threshold = thresholds[best_idx]
+        best_threshold = 0.45
 
-    print("\nBest Threshold:")
-    print(best_threshold)
-    y_pred = (y_prob >= best_threshold).astype(int)
+        print("\nBest Threshold:")
+        print(best_threshold)
+        y_pred = (y_prob >= best_threshold).astype(int)
 
-    #Evaluation
-    auc = roc_auc_score(y_test,y_prob)
-    precision = precision_score(y_test,y_pred)
-    recall = recall_score(y_test,y_pred)
-    f1 = f1_score(y_test,y_pred)
+        #Evaluation
+        auc = roc_auc_score(y_test,y_prob)
+        precision = precision_score(y_test,y_pred)
+        recall = recall_score(y_test,y_pred)
+        f1 = f1_score(y_test,y_pred)
 
-    print("\nROC-AUC Score:")
-    print(auc)
-    print("\nClassification Report:")
-    print(classification_report(y_test,y_pred))
-    print("\nPrecision:", precision)
-    print("Recall:", recall)
-    print("F1 Score:", f1)
+        print("\nROC-AUC Score:")
+        print(auc)
+        print("\nClassification Report:")
+        print(classification_report(y_test,y_pred))
+        print("\nPrecision:", precision)
+        print("Recall:", recall)
+        print("F1 Score:", f1)
 
-    mlflow.log_metric("roc_auc",auc)
-    mlflow.log_metric("precision",precision)
-    mlflow.log_metric("recall",recall)
-    mlflow.log_metric("f1_score",f1)
-    mlflow.sklearn.log_model(model,"lightgbm_model")
-    print("\nMLflow Run Completed")
+        mlflow.log_metric("roc_auc",auc)
+        mlflow.log_metric("precision",precision)
+        mlflow.log_metric("recall",recall)
+        mlflow.log_metric("f1_score",f1)
+        mlflow.sklearn.log_model(model,"lightgbm_model")
+        print("\nMLflow Run Completed")
 
     joblib.dump(model,"models/model.pkl") 
     # joblib.dump(X.columns.tolist(),"models/final_model.pkl")
     print("Model Saved")
 
-    train_model(df)
-
+    return df
 
 
